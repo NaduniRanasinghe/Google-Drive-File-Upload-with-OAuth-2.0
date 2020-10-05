4f904ca82c917c447e5001ec12a894e992199de0
NaduniRanasinghe <naduni.k.ranasinghe@gmail.com>
2020-10-05 23:26:45 +0530

    Initiated the project

---
+++ src/main/java/com/ssd/assignmentws/SsdOauthAssignmentWs2Application.java
@@ -0,0 +1,13 @@
+package com.ssd.assignmentws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdOauthAssignmentWs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdOauthAssignmentWs2Application.class, args);
+	}
+
+}
